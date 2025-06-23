3215ae6e2d2661f1500ed53e902b95f4c41c978e
Whyawww <wajinusantara05@gmail.com>
2025-06-24 00:32:00 +0700

    configure backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://deft-nasturtium-d58367.netlify.app/.netlify/functions/api";
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
